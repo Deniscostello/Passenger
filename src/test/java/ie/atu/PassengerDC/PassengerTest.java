54300311081fc99a3716b1f5420ebec81b05c49e
Denis Costello <G00396309@atu.ie>
2022-11-02 19:56:49 +0000

    Working test for Failure and Success of Title

--- src/test/java/ie/atu/PassengerDC/PassengerTest.java
+++ src/test/java/ie/atu/PassengerDC/PassengerTest.java
@@ -2,15 +2,28 @@ package ie.atu.PassengerDC;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
+    Passenger pass1;
 
     @BeforeEach
     void setUp() {
     }
 
+    @Test
+    void testTitleSuccess(){
+        Passenger pass1 = new Passenger("Mr", "Denis", "12345678910", "12345678", 20);
+        assertEquals("Mr", pass1.getTitle());
+    }
+
+    @Test
+    void testTitleFailure(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("M", "Denis", "12345678910", "12345678", 20);});
+        assertEquals("Please enter 'Mr' , 'Mrs' or 'Ms' ", exMessage.getMessage());
+    }
     @AfterEach
     void tearDown() {
     }
